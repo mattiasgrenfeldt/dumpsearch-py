42cf2f4611479c6dc9a27d2f7c9c5ec70ba27baf
Mattias Grenfeldt <mattiasgrenfeldt@gmail.com>
2019-06-13 14:17:49 +0200

    Added search functionality

--- src/db.py
+++ src/db.py
@@ -1,6 +1,8 @@
 import json, pymongo
 import exporter
 
+FIELDS = ["email", "username", "password", "hash", "firstname", "lastname", "phone", "dumpsource"]
+
 class DBConnection(exporter.Exporter):
 
     def __init__(self, configFileName):
@@ -11,12 +13,26 @@ class DBConnection(exporter.Exporter):
         self.config = config
         self.client = pymongo.MongoClient(config["ip"], config["port"])
         self.db = self.client["dumpsearch"]
-        self.collection = self.db["data"]
+        self.collectionName = "data"
+        self.collection = self.db[self.collectionName]
 
-    def search():
-        pass
+    def search(self, field, value, n=1, offset=0):
+        if field not in FIELDS:
+            print("[ERROR] Unknown search field %s" % field)
+            return (0, [])
+        nDocs = self.collection.count_documents({field: value}, limit=int(1e5))
+        res = self.collection.find({field: value})
+        return (nDocs, res[offset:offset + n])
 
     def buildIndexes(self):
+        print("[*] Building DB indexes. This might take a long time if they haven't been built yet.")
+        indexSizes = self.db.command("collStats", self.collectionName)["indexSizes"]
+        for f in FIELDS:
+            if f + "_1" not in indexSizes:
+                print("Creating index:", f)
+                self.collection.create_index(f, partialFilterExpression={f:{"$exists": True}})
+
+    def deleteIndexes(self):
         pass
 
     def exportEntry(self, entry):
--- src/dumpsearch.py
+++ src/dumpsearch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys, argparse, os
+import sys, argparse, os, json
 import os.path as path
 import parseformat, guesser, exporter, db
 import parser as prs
@@ -62,14 +62,52 @@ def guess():
 
 def search():
     parser = argparse.ArgumentParser(description='Search database.')
-    parser.add_argument('field', help="The field to search. Available:\n\tLIST")
+    parser.add_argument('field', help="The field to search. Available: email, username, password, hash, salt, hashtype, firstname, lastname, phone, dumpsource.")
     parser.add_argument('value', help="The value to search for.")
-    parser.add_argument('-n', default=10, help="The number of results to show.")
+    parser.add_argument('-c', "--config", default="dbconfig.json", help="DB config file. Default: dbconfig.json")
+    parser.add_argument('-n', default="10", help="The number of results to show.")
+    parser.add_argument('--offset', default="0", help="The number of entries to skip in beginning of result")
     parser.add_argument('-o', help="File to dump results in.")
+
     args = parser.parse_args(sys.argv[2:])
 
+    database = db.DBConnection(args.config)
+    database.buildIndexes()
 
-    
+    n = int(args.n)
+    offset = int(args.offset)
+
+    res = database.search(args.field, args.value, n, offset)
+
+    print()
+    if res[0] == int(1e5):
+        print("[*] Found >%d results" % res[0])
+    else:
+        print("[*] Found %d results" % res[0])
+    if args.o != None:
+        print("[*] Dumping result[%d:%d] to %s" % (offset, offset + n, args.o))
+        with open(args.o, "wb") as f:
+            res = list(res[1])
+            for i in range(len(res)):
+                res[i].pop("_id")
+            f.write(json.dumps(res).encode())
+    else:
+        res = list(res[1])
+        nRes = len(res)
+        colwidth = [max([len(res[i][field]) if field in res[i] else 0 for i in range(nRes)]) for field in parseformat.PARAMS_EXTENDED]
+        columns = []
+        for (width, field) in zip(colwidth, parseformat.PARAMS_EXTENDED):
+            if width != 0:
+                columns.append((max(width, len(field)), field))
+
+        print("[*] Showing result[%d:%d]" % (offset, offset + n))
+        print("+-" + "-+-".join(["-"*width for (width, _) in columns]) + "-+")
+        print("| " + " | ".join([col.ljust(width) for (width, col) in columns]) + " |")
+        print("+-" + "-+-".join(["-"*width for (width, _) in columns]) + "-+")
+        for r in res:
+            print("| " + " | ".join([r[col].ljust(width) for (width, col) in columns]) + " |")
+        print("+-" + "-+-".join(["-"*width for (width, _) in columns]) + "-+")
+        print("[*] Showing result[%d:%d]" % (offset, offset + n))
 
 def main():
     parser = argparse.ArgumentParser(usage="\nSubcommands:\n\tparse\n\tguess\n\tsearch")
